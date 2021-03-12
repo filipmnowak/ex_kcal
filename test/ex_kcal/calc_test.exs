b452be241d0bfe836a50b196828815b502defd68
Filip M. Nowak <github@temp.oneiroi.net>
2021-03-13 00:43:31 +0100

    formatting

--- test/ex_kcal/calc_test.exs
+++ test/ex_kcal/calc_test.exs
@@ -1,5 +1,4 @@
 defmodule ExKcal.CalcTest do
-
   use ExUnit.Case, async: true
   doctest ExKcal.Calc
 
@@ -12,7 +11,6 @@ defmodule ExKcal.CalcTest do
   alias ExKcal.Calc
 
   test "Check ExKcal.Calc.adjust_by_weight() for correct adjustments" do
-
     carxadopadolarinamodopehaphilaton = %Product{
       alcohols: %Alcohols{
         ethanol: {100, :g},
@@ -34,7 +32,7 @@ defmodule ExKcal.CalcTest do
         saturated: {3, :g},
         total: {6, :g}
       },
-      kcal: 1023552.0,
+      kcal: 1_023_552.0,
       minerals: %Minerals{
         calcium: {nil, :none},
         chloride: {nil, :none},
@@ -71,7 +69,7 @@ defmodule ExKcal.CalcTest do
         vitamin_b4: {nil, :none},
         vitamin_b6: {nil, :none},
         vitamin_b9: {nil, :none},
-        vitamin_c: {48213457.0, :g},
+        vitamin_c: {48_213_457.0, :g},
         vitamin_d: {nil, :none},
         vitamin_e: {nil, :none},
         vitamin_k: {nil, :none}
@@ -85,9 +83,9 @@ defmodule ExKcal.CalcTest do
         200,
         %ExKcal.Product{
           alcohols: %ExKcal.Alcohols{
-          ethanol: {160.0, :g},
-          other: {160.0, :g},
-          total: {320.0, :g}
+            ethanol: {160.0, :g},
+            other: {160.0, :g},
+            total: {320.0, :g}
           },
           brand: "",
           carbs: %ExKcal.Carbs{
@@ -104,7 +102,7 @@ defmodule ExKcal.CalcTest do
             saturated: {4.800000000000001, :g},
             total: {9.600000000000001, :g}
           },
-          kcal: 1637683.2000000002,
+          kcal: 1_637_683.2000000002,
           minerals: %ExKcal.Minerals{
             calcium: {nil, :none},
             chloride: {nil, :none},
@@ -141,92 +139,92 @@ defmodule ExKcal.CalcTest do
             vitamin_b4: {nil, :none},
             vitamin_b6: {nil, :none},
             vitamin_b9: {nil, :none},
-            vitamin_c: {77141531.2, :g},
+            vitamin_c: {77_141_531.2, :g},
             vitamin_d: {nil, :none},
             vitamin_e: {nil, :none},
             vitamin_k: {nil, :none}
           },
           volume: {nil, :none},
           weight: {200, :g}
-          }
-        },
-        {
-          34,
-          %ExKcal.Product{
-            alcohols: %ExKcal.Alcohols{
-              ethanol: {27.200000000000003, :g},
-              other: {27.200000000000003, :g},
-              total: {54.400000000000006, :g}
-            },
-            brand: "",
-            carbs: %ExKcal.Carbs{
-              dietary_fiber: {nil, :none},
-              other: {nil, :none},
-              starch: {nil, :none},
-              sugars: {nil, :none},
-              total: {nil, :none}
-            },
-            description: "",
-            fats: %ExKcal.Fats{
-              monounsaturated: {0.272, :g},
-              polyunsaturated: {0.544, :g},
-              saturated: {0.8160000000000001, :g},
-              total: {1.6320000000000001, :g}
-            },
-            kcal: 278406.14400000003,
-            minerals: %ExKcal.Minerals{
-              calcium: {nil, :none},
-              chloride: {nil, :none},
-              chromium: {0.272, :mg},
-              copper: {nil, :none},
-              fluoride: {nil, :none},
-              iodide: {nil, :none},
-              iodine: {nil, :none},
-              iron: {nil, :none},
-              magnesium: {nil, :none},
-              manganese: {nil, :none},
-              molybdenum: {nil, :none},
-              phosphorus: {nil, :none},
-              potassium: {nil, :none},
-              selenium: {nil, :none},
-              sodium: {nil, :none},
-              sulfur: {nil, :none},
-              zinc: {nil, :none}
-            },
-            name: "carxadopadolarinamodopehaphilaton",
-            note: "",
-            origin: [:planet_zordax13],
-            produced: [],
-            producer: "",
-            proteins: {nil, :none},
-            salt: {nil, :none},
-            vitamins: %ExKcal.Vitamins{
-              choline: {11.478400000000002, :mg},
-              vitamin_a: {nil, :none},
-              vitamin_b1: {nil, :none},
-              vitamin_b12: {nil, :none},
-              vitamin_b2: {nil, :none},
-              vitamin_b3: {nil, :none},
-              vitamin_b4: {nil, :none},
-              vitamin_b6: {nil, :none},
-              vitamin_b9: {nil, :none},
-              vitamin_c: {13114060.304000001, :g},
-              vitamin_d: {nil, :none},
-              vitamin_e: {nil, :none},
-              vitamin_k: {nil, :none}
-            },
-            volume: {nil, :none},
-            weight: {34, :g}
-            }
-          }
-        ]
+        }
+      },
+      {
+        34,
+        %ExKcal.Product{
+          alcohols: %ExKcal.Alcohols{
+            ethanol: {27.200000000000003, :g},
+            other: {27.200000000000003, :g},
+            total: {54.400000000000006, :g}
+          },
+          brand: "",
+          carbs: %ExKcal.Carbs{
+            dietary_fiber: {nil, :none},
+            other: {nil, :none},
+            starch: {nil, :none},
+            sugars: {nil, :none},
+            total: {nil, :none}
+          },
+          description: "",
+          fats: %ExKcal.Fats{
+            monounsaturated: {0.272, :g},
+            polyunsaturated: {0.544, :g},
+            saturated: {0.8160000000000001, :g},
+            total: {1.6320000000000001, :g}
+          },
+          kcal: 278_406.14400000003,
+          minerals: %ExKcal.Minerals{
+            calcium: {nil, :none},
+            chloride: {nil, :none},
+            chromium: {0.272, :mg},
+            copper: {nil, :none},
+            fluoride: {nil, :none},
+            iodide: {nil, :none},
+            iodine: {nil, :none},
+            iron: {nil, :none},
+            magnesium: {nil, :none},
+            manganese: {nil, :none},
+            molybdenum: {nil, :none},
+            phosphorus: {nil, :none},
+            potassium: {nil, :none},
+            selenium: {nil, :none},
+            sodium: {nil, :none},
+            sulfur: {nil, :none},
+            zinc: {nil, :none}
+          },
+          name: "carxadopadolarinamodopehaphilaton",
+          note: "",
+          origin: [:planet_zordax13],
+          produced: [],
+          producer: "",
+          proteins: {nil, :none},
+          salt: {nil, :none},
+          vitamins: %ExKcal.Vitamins{
+            choline: {11.478400000000002, :mg},
+            vitamin_a: {nil, :none},
+            vitamin_b1: {nil, :none},
+            vitamin_b12: {nil, :none},
+            vitamin_b2: {nil, :none},
+            vitamin_b3: {nil, :none},
+            vitamin_b4: {nil, :none},
+            vitamin_b6: {nil, :none},
+            vitamin_b9: {nil, :none},
+            vitamin_c: {13_114_060.304000001, :g},
+            vitamin_d: {nil, :none},
+            vitamin_e: {nil, :none},
+            vitamin_k: {nil, :none}
+          },
+          volume: {nil, :none},
+          weight: {34, :g}
+        }
+      }
+    ]
 
-      Enum.each(
-        adjustments_and_expected_results,
-        fn {adjustment, result} ->
-          assert result == Calc.adjust_by_weight(carxadopadolarinamodopehaphilaton, adjustment)
-        end
-      )
+    Enum.each(
+      adjustments_and_expected_results,
+      fn {adjustment, result} ->
+        assert result == Calc.adjust_by_weight(carxadopadolarinamodopehaphilaton, adjustment)
+      end
+    )
   end
 
   test "Check ExKcal.Calc.multiplier()" do
