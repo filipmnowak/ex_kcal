1e6bc42ab4dbdc6364a3149f77f90fb4613cf5c2
Filip M. Nowak <github@temp.oneiroi.net>
2021-03-13 00:44:54 +0100

    bump version to 0.0.3

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ExKcal.MixProject do
       package: package(),
       description: description(),
       app: :ex_kcal,
-      version: "0.0.3",
+      version: "0.0.4",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
