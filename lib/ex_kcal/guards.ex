9d80bea0278a13157ae9592c57e4528946bbce6e
Filip M. Nowak <github@temp.oneiroi.net>
2021-05-02 19:06:19 +0200

    remove commented code stolen from elixir source

--- lib/ex_kcal/guards.ex
+++ lib/ex_kcal/guards.ex
@@ -1,43 +1,4 @@
 defmodule ExKcal.Guards do
   defguard is_non_neg_number(value) when is_number(value) and value >= 0
   defguard is_non_neg_float(value) when is_float(value) and value >= 0
-
-  #  @doc since: "1.11.0", guard: true
-  #  defmacro any_of_structs(term, names) do
-  #    case __CALLER__.context do
-  #      nil ->
-  #        quote generated: true do
-  #          case unquote(name) do
-  #            name when is_atom(name) ->
-  #                  case unquote(term) do
-  #                    Enum.each(
-  #                      names,
-  #                      fn name ->
-  #                        %{__struct__: ^name} -> true
-  #                      end
-  #                    )
-  #                    _ -> false
-  #                  end
-  #            _ ->
-  #              raise ArgumentError
-  #          end
-  #        end
-  #
-  #      :match ->
-  #        invalid_match!(:is_struct)
-  #
-  #      :guard ->
-  #        quote do
-  #          is_map(unquote(term)) and
-  #          Enum.each(
-  #            names,
-  #            fn name ->
-  #              (is_atom(unquote(name)) or :fail) and
-  #              :erlang.is_map_key(:__struct__, unquote(term)) and
-  #              :erlang.map_get(:__struct__, unquote(term)) == unquote(name)
-  #            end
-  #          )
-  #        end
-  #    end
-  #  end
 end
