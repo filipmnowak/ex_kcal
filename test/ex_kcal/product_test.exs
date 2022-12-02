2d4ad9395be801ec899bbdf41549584275edf39d
Filip M. Nowak <github@temp.oneiroi.net>
2022-12-02 10:16:57 +0100

    tests tweaks

--- test/ex_kcal/product_test.exs
+++ test/ex_kcal/product_test.exs
@@ -6,7 +6,7 @@ defmodule ExKcal.ProductTest do
   alias ExKcal.Carbs
   alias ExKcal.Fats
 
-  test "Check if ExKcal.Product.sum returns right result" do
+  test "Check if Product.sum returns right result" do
     test_lentil = %Product{
       amount: {100.0, :g},
       proteins: {25.0, :g},
@@ -48,15 +48,15 @@ defmodule ExKcal.ProductTest do
       producer: "X Farmer"
     }
 
-    test_lentilopotat = %ExKcal.Product{
-      carbs: %ExKcal.Carbs{
+    test_lentilopotat = %Product{
+      carbs: %Carbs{
         dietary_fiber: {12.899999999999999, :g},
         other: {63.0, :g},
         starch: {nil, :none},
         sugars: {2.7800000000000002, :g},
         total: {92.7, :g}
       },
-      fats: %ExKcal.Fats{
+      fats: %Fats{
         monounsaturated: {0.0, :g},
         polyunsaturated: {0.04, :g},
         saturated: {0.03, :g},
@@ -71,15 +71,77 @@ defmodule ExKcal.ProductTest do
   end
 
   test "Product.encode/1" do
-    assert(
-      "{\"alcohols\":{\"ethanol\":[null,\"none\"],\"other\":[null,\"none\"],\"total\":[null,\"none\"]},\"amount\":[null,\"none\"],\"brand\":\"\",\"carbs\":{\"dietary_fiber\":[null,\"none\"],\"other\":[null,\"none\"],\"starch\":[null,\"none\"],\"sugars\":[null,\"none\"],\"total\":[null,\"none\"]},\"description\":\"\",\"fats\":{\"monounsaturated\":[null,\"none\"],\"polyunsaturated\":[null,\"none\"],\"saturated\":[null,\"none\"],\"total\":[null,\"none\"]},\"kcal\":0.0,\"minerals\":{\"calcium\":[null,\"none\"],\"chloride\":[null,\"none\"],\"chromium\":[null,\"none\"],\"copper\":[null,\"none\"],\"fluoride\":[null,\"none\"],\"iodide\":[null,\"none\"],\"iodine\":[null,\"none\"],\"iron\":[null,\"none\"],\"magnesium\":[null,\"none\"],\"manganese\":[null,\"none\"],\"molybdenum\":[null,\"none\"],\"phosphorus\":[null,\"none\"],\"potassium\":[null,\"none\"],\"selenium\":[null,\"none\"],\"sodium\":[null,\"none\"],\"sulfur\":[null,\"none\"],\"zinc\":[null,\"none\"]},\"name\":\"\",\"note\":\"\",\"origin\":[],\"produced\":[],\"producer\":\"\",\"proteins\":[null,\"none\"],\"salt\":[null,\"none\"],\"vitamins\":{\"choline\":[null,\"none\"],\"vitamin_a\":[null,\"none\"],\"vitamin_b1\":[null,\"none\"],\"vitamin_b12\":[null,\"none\"],\"vitamin_b2\":[null,\"none\"],\"vitamin_b3\":[null,\"none\"],\"vitamin_b4\":[null,\"none\"],\"vitamin_b6\":[null,\"none\"],\"vitamin_b9\":[null,\"none\"],\"vitamin_c\":[null,\"none\"],\"vitamin_d\":[null,\"none\"],\"vitamin_e\":[null,\"none\"],\"vitamin_k\":[null,\"none\"]}}" ===
-        Product.encode(%Product{})
-    )
+    assert(Product.encode(%Product{}) |> Product.decode() == %Product{})
   end
 
   test "Product.decode/1" do
-    input =
-      "{\"alcohols\":{\"ethanol\":[null,\"none\"],\"other\":[null,\"none\"],\"total\":[null,\"none\"]},\"amount\":[null,\"none\"],\"brand\":\"\",\"carbs\":{\"dietary_fiber\":[null,\"none\"],\"other\":[null,\"none\"],\"starch\":[null,\"none\"],\"sugars\":[null,\"none\"],\"total\":[null,\"none\"]},\"description\":\"\",\"fats\":{\"monounsaturated\":[null,\"none\"],\"polyunsaturated\":[null,\"none\"],\"saturated\":[null,\"none\"],\"total\":[null,\"none\"]},\"kcal\":0.0,\"minerals\":{\"calcium\":[null,\"none\"],\"chloride\":[null,\"none\"],\"chromium\":[null,\"none\"],\"copper\":[null,\"none\"],\"fluoride\":[null,\"none\"],\"iodide\":[null,\"none\"],\"iodine\":[null,\"none\"],\"iron\":[null,\"none\"],\"magnesium\":[null,\"none\"],\"manganese\":[null,\"none\"],\"molybdenum\":[null,\"none\"],\"phosphorus\":[null,\"none\"],\"potassium\":[null,\"none\"],\"selenium\":[null,\"none\"],\"sodium\":[null,\"none\"],\"sulfur\":[null,\"none\"],\"zinc\":[null,\"none\"]},\"name\":\"\",\"note\":\"\",\"origin\":[],\"produced\":[],\"producer\":\"\",\"proteins\":[null,\"none\"],\"salt\":[null,\"none\"],\"vitamins\":{\"choline\":[null,\"none\"],\"vitamin_a\":[null,\"none\"],\"vitamin_b1\":[null,\"none\"],\"vitamin_b12\":[null,\"none\"],\"vitamin_b2\":[null,\"none\"],\"vitamin_b3\":[null,\"none\"],\"vitamin_b4\":[null,\"none\"],\"vitamin_b6\":[null,\"none\"],\"vitamin_b9\":[null,\"none\"],\"vitamin_c\":[null,\"none\"],\"vitamin_d\":[null,\"none\"],\"vitamin_e\":[null,\"none\"],\"vitamin_k\":[null,\"none\"]}}"
+    input = """
+    {
+      "alcohols": {
+      "ethanol": [null, "none"],
+      "other": [null, "none"],
+      "total": [null, "none"]
+    },
+      "amount": [null, "none"],
+      "brand": "",
+      "carbs": {
+      "dietary_fiber": [null, "none"],
+      "other": [null, "none"],
+      "starch": [null, "none"],
+      "sugars": [null, "none"],
+      "total": [null, "none"]
+    },
+    "description": "",
+    "fats": {
+      "monounsaturated": [null, "none"],
+      "polyunsaturated": [null, "none"],
+      "saturated": [null, "none"],
+      "total": [null, "none"]
+    },
+    "kcal": 0.0,
+    "minerals": {
+      "calcium": [null, "none"],
+      "chloride": [null, "none"],
+      "chromium": [null, "none"],
+      "copper": [null, "none"],
+      "fluoride": [null, "none"],
+      "iodide": [null, "none"],
+      "iodine": [null, "none"],
+      "iron": [null, "none"],
+      "magnesium": [null, "none"],
+      "manganese": [null, "none"],
+      "molybdenum": [null, "none"],
+      "phosphorus": [null, "none"],
+      "potassium": [null, "none"],
+      "selenium": [null, "none"],
+      "sodium": [null, "none"],
+      "sulfur": [null, "none"],
+      "zinc": [null, "none"]
+    },
+    "name": "",
+    "note": "",
+    "origin": [],
+    "produced": [],
+    "producer": "",
+    "proteins": [null, "none"],
+    "salt": [null, "none"],
+    "vitamins": {
+      "choline": [null, "none"],
+      "vitamin_a": [null, "none"],
+      "vitamin_b1": [null, "none"],
+      "vitamin_b12": [null, "none"],
+      "vitamin_b2": [null, "none"],
+      "vitamin_b3": [null, "none"],
+      "vitamin_b4": [null, "none"],
+      "vitamin_b6": [null, "none"],
+      "vitamin_b9": [null, "none"],
+      "vitamin_c": [null, "none"],
+      "vitamin_d": [null, "none"],
+      "vitamin_e": [null, "none"],
+      "vitamin_k": [null, "none"]
+      }
+    }
+    """
 
     assert(Product.decode(input) === %Product{})
   end
